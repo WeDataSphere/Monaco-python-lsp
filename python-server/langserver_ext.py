af5fe7a56f9378685750da5cc4f07090b66c9b30
yuankang134 <373346037@qq.com>
2022-11-15 14:12:22 +0800

    释放子进程

--- python-server/langserver_ext.py
+++ python-server/langserver_ext.py
@@ -2,18 +2,22 @@ import json
 import logging
 import subprocess
 import threading
+import os
 
 from tornado import ioloop, process, web, websocket
 
 from pyls_jsonrpc import streams
+
+from lxpy import copy_headers_dict
 
 log = logging.getLogger(__name__)
 
 
 class LanguageServerWebSocketHandler(websocket.WebSocketHandler):
     """Setup tornado websocket handler to host an external language server."""
-
+    log.info("=========LanguageServerWebSocketHandler=======")
     writer = None
+    map_catch = {}
 
     def open(self, *args, **kwargs):
         log.info("Spawning pyls subprocess")
@@ -24,6 +28,17 @@ class LanguageServerWebSocketHandler(websocket.WebSocketHandler):
             stdin=subprocess.PIPE,
             stdout=subprocess.PIPE
         )
+
+        cookie = self.absolve_cookie()
+        log.info("====================headers=================")
+        log.info(cookie)
+        log.info("====================headers=================")
+        global map_catch
+        self.map_catch[cookie] = proc.pid
+
+        log.info("================pid================")
+        log.info(self.map_catch)
+        log.info("================pid================")
 
         # Create a writer that formats json messages with the correct LSP headers
         self.writer = streams.JsonRpcStreamWriter(proc.stdin)
@@ -47,10 +62,28 @@ class LanguageServerWebSocketHandler(websocket.WebSocketHandler):
     def check_origin(self, origin):
         return True
 
+    def on_close(self) -> None:
+        try:
+            close_cookie = self.absolve_cookie()
+            log.info(close_cookie)
+            log.info(self.map_catch)
+            os.kill(int(self.map_catch[close_cookie]), 9)
+            log.info(True)
+            del self.map_catch[close_cookie]
+        except:
+            log.info(False)
+        log.info("=============on_close==============")
+
+    def absolve_cookie(self):
+        header_dict = copy_headers_dict(str(self.request.headers))
+        return header_dict['Cookie']
+
 
 if __name__ == "__main__":
+    print("=========main=========")
     app = web.Application([
         (r"/python", LanguageServerWebSocketHandler),
     ])
     app.listen(3001)
     ioloop.IOLoop.current().start()
+    print("=========main=========")
